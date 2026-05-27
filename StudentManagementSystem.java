251c317098e9fe1c8697b2409a485db9395f5f89
MD Dharsh <dharshinimuruganantham18@gmail.com>
2026-05-27 13:02:15 +0530

    Cleaned project structure

--- StudentManagementSystem.java
+++ StudentManagementSystem.java
@@ -9,17 +9,12 @@ public class StudentJDBC {
 
         try {
 
-            // 1. Load Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-
-            // 2. Create Connection
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/studentdb",
                 "root",
                 "root"
             );
-
-            // 3. Take Input
             System.out.println("Enter ID:");
             int id = sc.nextInt();
             sc.nextLine();
@@ -30,22 +25,18 @@ public class StudentJDBC {
             System.out.println("Enter Department:");
             String dept = sc.nextLine();
 
-            // 4. SQL Query
             String query = "insert into students values(?,?,?)";
 
-            // 5. Prepare Statement
             PreparedStatement ps = con.prepareStatement(query);
 
             ps.setInt(1, id);
             ps.setString(2, name);
             ps.setString(3, dept);
 
-            // 6. Execute Query
             ps.executeUpdate();
 
             System.out.println("Data Inserted Successfully");
 
-            // 7. Close Connection
             con.close();
 
         } catch(Exception e) {
